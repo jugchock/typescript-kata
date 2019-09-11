021e009ce6677e7f2a281e2cc5deec3aee511f8c
Chris Chock <chris.chock@polaris.com>
2019-09-11 12:47:09 -0500

    Added tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,13 +1,56 @@
-import { test } from './index';
+import { returnValue, add, ifTest, switchTest, forTest } from './index';
 
-describe('Index', () => {
+describe('Return a value', () => {
   it('should return the value passed in', () => {
-    const result = test('foo');
+    const result = returnValue('foo');
     expect(result).toBe('foo');
   });
 
   it('should return value when undefined is passed in', () => {
-    const result = test(undefined);
+    const result = returnValue(undefined);
     expect(result).toBe(undefined);
+  });
+});
+
+describe('Add two values', () => {
+  it('should add two values together', () => {
+    const sum = add(7, 2);
+    expect(sum).toBe(9);
+  });
+});
+
+describe('If test', () => {
+  it('should return true', () => {
+    const result = ifTest('true');
+    expect(result).toBe(true);
+  });
+
+  it('should return false', () => {
+    const result = ifTest('false');
+    expect(result).toBe(false);
+  });
+});
+
+describe('Switch test', () => {
+  it('should return "bar"', () => {
+    const result = switchTest('foo');
+    expect(result).toBe('bar');
+  });
+
+  it('should return "two"', () => {
+    const result = switchTest('one');
+    expect(result).toBe('two');
+  });
+
+  it('should return "none"', () => {
+    const result = switchTest('nothing');
+    expect(result).toBe('none');
+  });
+});
+
+describe('For test', () => {
+  it('should return "testtest"', () => {
+    const result = forTest('test', 2);
+    expect(result).toBe('testtest');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export function test(input: any): any {
+// return the value passed in
+export function returnValue(input: any): any {
+}
 
+// add the two values passed in
+export function add(inputA: number, inputB: number): any {
+}
+
+// return true if input is the string "true", otherwise return false
+export function ifTest(input: string): boolean {
+}
+
+// return "bar" if "foo" is passed in, "two" if "one" is passed and "none" otherwise, use switch statement
+export function switchTest(input: string): string {
+}
+
+// return {inString} concatenated {times} times  
+export function forTest(inString: string, times: number) {
 }
