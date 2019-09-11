e13ec96c099f455d2ced7836854135cefd895c12
Chris Chock <chris.chock@polaris.com>
2019-09-11 12:56:17 -0500

    comment out unused tests and functions

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,4 @@
-import { returnValue, add, ifTest, switchTest, forTest } from './index';
+import { returnValue } from './index';
 
 describe('Return a value', () => {
   it('should return the value passed in', () => {
@@ -12,45 +12,49 @@ describe('Return a value', () => {
   });
 });
 
-describe('Add two values', () => {
-  it('should add two values together', () => {
-    const sum = add(7, 2);
-    expect(sum).toBe(9);
-  });
-});
+// describe('Add two values', () => {
+//   it('should add two values together', () => {
+//     const sum = add(7, 2);
+//     expect(sum).toBe(9);
+//   });
+// });
 
-describe('If test', () => {
-  it('should return true', () => {
-    const result = ifTest('true');
-    expect(result).toBe(true);
-  });
+// describe('If test', () => {
+//   it('should return true', () => {
+//     const result = ifTest('true');
+//     expect(result).toBe(true);
+//   });
 
-  it('should return false', () => {
-    const result = ifTest('false');
-    expect(result).toBe(false);
-  });
-});
+//   it('should return false', () => {
+//     const result = ifTest('false');
+//     expect(result).toBe(false);
+//   });
+// });
 
-describe('Switch test', () => {
-  it('should return "bar"', () => {
-    const result = switchTest('foo');
-    expect(result).toBe('bar');
-  });
+// describe('Switch test', () => {
+//   it('should return "bar"', () => {
+//     const result = switchTest('foo');
+//     expect(result).toBe('bar');
+//   });
 
-  it('should return "two"', () => {
-    const result = switchTest('one');
-    expect(result).toBe('two');
-  });
+//   it('should return "two"', () => {
+//     const result = switchTest('one');
+//     expect(result).toBe('two');
+//   });
 
-  it('should return "none"', () => {
-    const result = switchTest('nothing');
-    expect(result).toBe('none');
-  });
-});
+//   it('should return "none"', () => {
+//     const result = switchTest('nothing');
+//     expect(result).toBe('none');
+//   });
+// });
 
-describe('For test', () => {
-  it('should return "testtest"', () => {
-    const result = forTest('test', 2);
-    expect(result).toBe('testtest');
-  });
-});
+// describe('For test', () => {
+//   it('should return "testtest"', () => {
+//     const result = forTest('test', 2);
+//     expect(result).toBe('testtest');
+//   });
+// });
+
+// describe('Class test', () => {
+
+// })
