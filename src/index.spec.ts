0e3c404ef262913609c2d8d1b73fa9878c3637c4
Chris Chock <chris.chock@polaris.com>
2019-10-31 13:34:57 -0500

    Add for answer

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,60 +1,7 @@
 import {
-  add,
   forTest,
-  ifTest,
-  isPalindromeFor,
-  isPalindromeRecursive,
-  returnValue,
-  switchTest,
+  isPalindromeFor
 } from './index';
-
-describe('Return a value', () => {
-  it('should return the value passed in', () => {
-    const result = returnValue('foo');
-    expect(result).toBe('foo');
-  });
-
-  it('should return value when undefined is passed in', () => {
-    const result = returnValue(undefined);
-    expect(result).toBe(undefined);
-  });
-});
-
-describe('Add two values', () => {
-  it('should add two values together', () => {
-    const sum = add(7, 2);
-    expect(sum).toBe(9);
-  });
-});
-
-describe('If test', () => {
-  it('should return true', () => {
-    const result = ifTest('true');
-    expect(result).toBe(true);
-  });
-
-  it('should return false', () => {
-    const result = ifTest('false');
-    expect(result).toBe(false);
-  });
-});
-
-describe('Switch test', () => {
-  it('should return "bar"', () => {
-    const result = switchTest('foo');
-    expect(result).toBe('bar');
-  });
-
-  it('should return "two"', () => {
-    const result = switchTest('one');
-    expect(result).toBe('two');
-  });
-
-  it('should return "none"', () => {
-    const result = switchTest('nothing');
-    expect(result).toBe('none');
-  });
-});
 
 describe('Simple for test', () => {
   it('should return "testtest"', () => {
@@ -80,25 +27,3 @@ describe('Palindrome for test', () => {
     expect(isPalindromeFor('abcdba')).toBe(false);
   });
 });
-
-describe('Palindrome recursive test', () => {
-  it('should return true', () => {
-    expect(isPalindromeRecursive('')).toBe(true);
-    expect(isPalindromeRecursive('a')).toBe(true);
-    expect(isPalindromeRecursive('aa')).toBe(true);
-    expect(isPalindromeRecursive('aba')).toBe(true);
-    expect(isPalindromeRecursive('abba')).toBe(true);
-    expect(isPalindromeRecursive('abccba')).toBe(true);
-  });
-
-  it('should return false', () => {
-    expect(isPalindromeRecursive('ab')).toBe(false);
-    expect(isPalindromeRecursive('abc')).toBe(false);
-    expect(isPalindromeRecursive('abca')).toBe(false);
-    expect(isPalindromeRecursive('abcdba')).toBe(false);
-  });
-});
-
-// describe('Class test', () => {
-
-// })
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,10 @@
 // Checkout the following branches to get the answers, next test or previous test.
 //
-// current: 
-// answer: 
-// next: 
-// previous: 
+// current: for
+// answer: for-answer
+// next: recursive
+// previous: switch
 
-// return the value passed in
-export function returnValue(input: any): any {
-  return input;
-}
-
-// add the two values passed in
-export function add(inputA: number, inputB: number): any {
-  return inputA + inputB;
-}
-
-// return true if input is the string "true", otherwise return false
-// use an if statement
-export function ifTest(input: string): boolean {
-  if (input === 'true') {
-    return true;
-  }
-
-  return false;
-}
-
-// return "bar" if "foo" is passed in, "two" if "one" is passed and "none" otherwise
-// use a switch statement
-export function switchTest(input: string): string {
-  switch (input) {
-    case 'foo':
-      return 'bar';
-    case 'one':
-      return 'two';
-    default:
-      return 'none';
-  }
-}
 
 // return {inString} concatenated {times} times
 // use a for loop
@@ -65,90 +33,3 @@ export function isPalindromeFor(test: string): boolean {
 
   return result;
 }
-
-export function isPalindromeRecursive(test: string): boolean {
-  if (test.length < 2) { return true; }
-
-  const doFirstAndLastMatch = test[0] === test[test.length - 1];
-  const nextStringTest = test.slice(1, test.length - 1);
-
-  return doFirstAndLastMatch && isPalindromeRecursive(nextStringTest);
-}
-
-// IIFE
-(function IIFE() {
-  let foo = 'foo';
-
-})();
-
-
-let FooModel = (function IIFE() {
-  let foo = 'foo';
-
-  return function constructor() {
-    return Object.create({
-      foo: {
-        get: function getFoo() {
-          return foo;
-        },
-      },
-
-    });
-  }
-})
-
-// let myFoo = new FooModel();
-
-export class ClassTest {
-  public foo = 'foo';
-
-  public returnFoo() {
-    return this.foo;
-  }
-}
-
-let classTest = new ClassTest();
-
-
-// class with constructor
-export class ConstructorTest {
-  public name: string;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  public returnName() {
-    return this.name;
-  }
-}
-
-let constructorTest = new ConstructorTest('joe');
-constructorTest.name = 'not joe';
-constructorTest.returnName();
-
-
-export class PrivatePropertyTest {
-  private name: string;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  public returnName() {
-    return this.name;
-  }
-}
-
-let privatePropertyTest = new PrivatePropertyTest('joe');
-
-Object.create({
-
-});
-
-
-
-
-// class with private method?
-// class with private property?
-
