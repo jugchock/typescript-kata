07baf88ba50496a147c6d095bb0c3ddcb775a5e4
Chris Chock <chris.chock@polaris.com>
2019-10-31 13:30:38 -0500

    Add current branch info placeholder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 // Checkout the following branches to get the answers, next test or previous test.
 //
-// answer:
-// next:
-// previous:
+// current: 
+// answer: 
+// next: 
+// previous: 
 
 // return the value passed in
 export function returnValue(input: any): any {
